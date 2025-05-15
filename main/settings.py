d85a1cc73a4f0de3424f454dd07031ba66217aec
DavidRiccio <dricciodeabreu1@gmail.com>
2025-05-15 12:36:49 +0100

    Añadimos django-extensions

--- main/settings.py
+++ main/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'bookings.apps.BookingsConfig',
     'corsheaders',
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
